2f449b3855b5b30d7253d3f387d4a54ccdebd61c
Fakhruddin Saupe <30628450+Fakhruddin90@users.noreply.github.com>
2020-10-16 00:12:54 +0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import Login from "./Login";
+import Input from "./Input";
+
+var isLoggedIn = false;
+
+// function renderConditionally() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
+
+// ternary operator ////
+// isLoggedIn ? <h1>Hello</h1> : <Login />
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
   );
 }
 
